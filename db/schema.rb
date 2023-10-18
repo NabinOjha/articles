bba57e7e5465a4a373eed01cd59ab2c16f86193c
NabinOjha <nabinojha47@gmail.com>
2023-10-18 20:48:10 +0545

    Adds ownership of app inside of docker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_143551) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_18_143921) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_14_143551) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.text "phone_no", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 rm -f tmp/pids/server.pid
+useradd -m -p '*' nabinojha
+chown -R nabinojha:nabinojha .
 bundle
 rails db:create
 rails db:migrate
